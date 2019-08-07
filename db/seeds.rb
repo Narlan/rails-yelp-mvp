463f5543dbd6abc7a917b640b0fafce02e41ab94
Adam Castro Guzman <castroguzman033@gmail.com>
2019-08-07 14:11:38 +0200

    Green rake, but a bug yet to be fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.delete_all
+resto = Restaurant.new(name: 'La crevette verdâtre', address: 'Loin de moi', category: 'french')
+resto.save!
+resto.reviews.create(content: "Super", rating: 5)
+resto.save!
+resto = Restaurant.new(name: 'Chez le stagiaire libre', address: 'Nul part', category: 'french')
+resto.save!
+resto = Restaurant.new(name: 'Mcdo', address: 'Pas loin', category: 'french')
+resto.save!
+resto = Restaurant.new(name: 'La cuisine pas chère', address: 'Quelque part', category: 'french')
+resto.save!
+resto = Restaurant.new(name: 'Au sanglier grillé', address: 'En bretagne', category: 'french')
+resto.save!
