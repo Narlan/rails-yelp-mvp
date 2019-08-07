84afec7ed4aecc4e9f4241b9033923f3fd738672
Adam Castro Guzman <castroguzman033@gmail.com>
2019-08-07 14:23:55 +0200

    Fixed and done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,12 @@
 class ReviewsController < ApplicationController
-
   def new
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
   end
 
   def create
-    @review =Review.new(review_params)
+    @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
     @review.restaurant = Restaurant.find(params[:restaurant_id])
     if @review.valid?
       @review.save
@@ -16,10 +16,9 @@ class ReviewsController < ApplicationController
     end
   end
 
- private
+  private
 
- def review_params
-  params.require(:review).permit(:content, :rating)
- end
-
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
 end
